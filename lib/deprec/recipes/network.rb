e5adc72277f040c1715488e2381c8283a6ba43c8
mbailey <mbailey@479ab18a-db23-0410-b897-fa3724b950b0>
2008-04-17 08:50:22 +0000

    updated tasks for configuring network interfaces
    git-svn-id: svn://scm.deprecated.org/deprec/trunk@450 479ab18a-db23-0410-b897-fa3724b950b0

--- lib/deprec/recipes/network.rb
+++ lib/deprec/recipes/network.rb
@@ -2,20 +2,43 @@
 Capistrano::Configuration.instance(:must_exist).load do 
   namespace :deprec do
     namespace :network do
+            
+      set(:network_number_of_ports) { 
+        Capistrano::CLI.ui.ask "Number of network ports" do |q|
+          q.default = 1
+        end 
+      }
       
-      set(:hostname) { 
+      set(:network_interfaces) {
+        foo = {}
+        network_number_of_ports.to_i.times do |port|
+          foo[port] = {}
+          foo[port][:address] = Capistrano::CLI.ui.ask "address" do |q|
+            q.default = "192.168.#{port+1}.10"
+          end
+          foo[port][:netmask] = Capistrano::CLI.ui.ask "netmask" do |q|
+            q.default = '255.255.255.0'
+          end
+          foo[port][:broadcast] = Capistrano::CLI.ui.ask "broadcast" do |q|
+            q.default = "192.168.#{port+1}.255"
+          end
+
+        end
+        foo
+      }
+      set(:network_hostname) { 
         Capistrano::CLI.ui.ask "hostname" do |q|
           # q.validate = /add hostname validation here/
         end 
       } 
-      set(:eth0_ip) { 
-        Capistrano::CLI.ui.ask "eth0 ip address" do |q|
-          # q.validate = /add ip addr validation here/
+      set(:network_gateway) { 
+        Capistrano::CLI.ui.ask "gateway" do |q|
+          q.default = '192.168.1.1'
         end 
       }
-      set(:eth1_ip) { 
-        Capistrano::CLI.ui.ask "eth1 ip address" do |q|
-          # q.validate = /add ip addr validation here/
+      set(:network_dns_nameservers) { 
+        Capistrano::CLI.ui.ask "dns nameservers (separated by spaces)" do |q|
+          q.default = '203.8.183.1 4.2.2.1'
         end 
       }
       
@@ -37,16 +60,16 @@ Capistrano::Configuration.instance(:must_exist).load do
          :owner => 'root:root'}
     
        ]
-      
+       
       desc "Update system networking configuration"
-      task :config, :roles => :vmgz do
+      task :config do
         SYSTEM_CONFIG_FILES[:network].each do |file|
           deprec2.render_template(:network, file.merge(:remote=>true))
         end
       end
       
       desc "Restart network interface"
-      task :restart, :roles => :vmgz do
+      task :restart do
         sudo '/etc/init.d/networking restart'
       end
       
